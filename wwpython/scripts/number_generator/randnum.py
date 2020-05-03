d94f8b9b8bf112519b5122c04a24cfecc18b7b77
birdman0030 <kurtbird1@gmail.com>
2020-05-02 23:30:49 -0600

    Created randnum.py to generate and sort integers, move parser.py to own directory for readablity

---
+++ wwpython/scripts/number_generator/randnum.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def randomNumGen():
+    """
+    Generates a list of 500 random numbers between 0-1000.
+
+    :return generated: a list of integers.
+    """
+    generated = [randint(0, 1000) for i in range(500)]
+    return generated
+
+
+def findNumPosition(nums, val):
+    """
+    Converts a list of integers to a set and returns the nth value.
+
+    :param nums: a list of integers.
+    :param val: a nth value to find in a list of integers.
+    """
+    gen_sort = set(nums)
+    return sorted(gen_sort)[val]
+
+
+if __name__ == "__main__":
+    container = randomNumGen()
+    print(findNumPosition(container, 10))
