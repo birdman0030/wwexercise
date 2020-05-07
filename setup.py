3fdce2108d896f94754aadb3d918bc3ce1b6274e
birdman0030 <kurtbird1@gmail.com>
2020-05-06 22:40:26 -0600

    fixed path to README.md in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from distutils.core import setup
-with open("README.md", "r") as f:
-      long_description = f.read()
+import os
 
-setup(name='wwexercise',
-    version='v0.4',
+from distutils.core import setup
+from configuration import ROOT_DIR
+
+with open(os.path.join(ROOT_DIR, "README.md"), "r") as f:
+    long_description = f.read()
+
+setup(
+    name='wwexercise',
+    version='v0.5',
     description='Weight Watchers coding exercise in python',
     url='https://github.com/birdman0030/wwexercise',
     author='Kurt Bird',
     author_email='kurtbird1@gmail.com',
     license='GNU',
-    packages=["wwexercise"],
+    packages="wwexercise",
     install_requires=["selenium==3.141.0",
                       "pytest==5.4.1",
                       ],
