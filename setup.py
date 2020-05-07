11e5d458a835eaba7f0f34e424d1dd6b27f06104
birdman0030 <kurtbird1@gmail.com>
2020-05-06 21:49:54 -0600

    updated setup.py - removed intended_audiance field

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-from setuptools import setup
+from distutils.core import setup
 with open("README.md", "r") as f:
       long_description = f.read()
 
 setup(name='wwpython',
-      version='0.1',
-      description='Weight Watchers coding exercise in python3',
-      url='https://github.com/birdman0030/wwpython',
-      author='Kurt Bird',
-      author_email='kurtbird1@gmail.com',
-      license='GNU',
-      packages=[
-            "selenium",
-      ],
-      )
+    version='0.1',
+    description='Weight Watchers coding exercise in python3',
+    url='https://github.com/birdman0030/wwpython',
+    download_url=[
+        'https://github.com/birdman0030/wwexercise/archive/0.1.tar.gz',
+        'https://github.com/birdman0030/wwexercise/archive/0.1.zip'
+        ],
+    author='Kurt Bird',
+    author_email='kurtbird1@gmail.com',
+    license='GNU',
+    packages=["wwexercise"],
+    install_requires=["selenium==3.141.0",
+                      "pytest==5.4.1",
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6.4',
+        ],
+)
